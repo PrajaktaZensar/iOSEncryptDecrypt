5a8ffcbdf0bd08e28a99b48c28ed6bcd8cb64258
PrajaktaZensar <108938592+PrajaktaZensar@users.noreply.github.com>
2022-08-08 15:39:20 +0530

    comments and documentation added

--- iOSEncryptDecryptLib/EncryptDecryptFile.swift
+++ iOSEncryptDecryptLib/EncryptDecryptFile.swift
@@ -10,9 +10,14 @@ import CommonCrypto
 import CryptoKit
 
 
-//-------- For Cryptokit-----------
 @available(iOS 13.0, *)
 public class CryptoKitClass {
+    
+/**
+ 
+ - Parameter passowrdString: User's message data.
+ 
+ */
 
     var passowrdString: String!
     let randomKey = SymmetricKey(size: .bits256)
@@ -23,14 +28,24 @@ public class CryptoKitClass {
             self.passowrdString = passowrdString
     }
     
-/*------------------ENCRYPTING/DECRYPTING DATA---------------------------
+/* ------------------ENCRYPTING/DECRYPTING DATA---------------------------
 
---------------- You can encrypt the contents of super secret msg--------
---------------- AES with Galois/Counter Mode (AES-GCM) provides both authenticated encryption (confidentiality and authentication) and the ability to check the integrity and authentication of additional authenticated data (AAD) that is sent in the clear.
+ ## You can encrypt the contents of super secret msg.
+ 
+  ## AES with Galois/Counter Mode (AES-GCM) provides both authenticated encryption (confidentiality and authentication) and the ability to check the integrity and authentication of additional authenticated data (AAD) that is sent in the clear.
 
-------------------ENCRYPTING/DECRYPTING DATA---------------------------*/
+------------------ENCRYPTING/DECRYPTING DATA--------------------------- */
+
+    /**
+     
+     Creates a AESencryptFunc for AES Encryption.
+     
+     - Parameter passowrdString: data which you want to encrypt.
+     - Returns: Encrypted data into string format `encryptedData`.
+     
+    */
     
-    
+    // MARK: - AES Encryption
     public func AESencryptFunc(passowrdString: String) throws -> String {
         let textData = passowrdString.data(using: .utf8)!
         let encrypted = try AES.GCM.seal(textData, using: randomKey)
@@ -39,7 +54,16 @@ public class CryptoKitClass {
     }
     
     
+    /**
+     
+     Creates a AESdecryptFunc for AES Decryption.
+     
+     - Returns: Decrypted string `text`.
+     
+    */
+    
 
+    // MARK: - AES Decryption
     public func AESdecryptFunc() -> String {
         do {
             guard let data = Data(base64Encoded: try encryptedData) else {
@@ -58,37 +82,52 @@ public class CryptoKitClass {
             return "Error decrypting message: \(error.localizedDescription)"
         }
     }
-    
-//------------------------------------------------------------------------------
-/*----------------------------AUTHENTICATE--------------------------------------
+ 
+/**----------------------------AUTHENTICATE--------------------------------------
 
---------------- Hash-based Message Authentication Code
---------------- The HMAC process mixes a secret key with the message data and hashes the result. The hash value is mixed with the secret key again, and then hashed a second time.
+ ## Hash-based Message Authentication Code.
+ 
+  ## The HMAC process mixes a secret key with the message data and hashes the result. The hash value is mixed with the secret key again, and then hashed a second time.
 
 ----------------------------AUTHENTICATE--------------------------------------*/
+    
+    /**
+     
+     Creates a authenticateHmacSHA512CryptoKit for Hash-based Message Authentication.
+     
+     - Parameter passowrdString: data which you want to authenticate.
+     - Returns: Authenticated data into string format `passwordAuthenticateDigest`.
+     
+    */
 
-        // CrytoKit
+    
         public func authenticateHmacSHA512CryptoKit() -> String? {
             // Create the hash
             let passwordData = passowrdString.data(using: .utf8)!
             let symmetricKey = SymmetricKey(data: passwordData)
-            let passwordHashDigest = HMAC<SHA512>.authenticationCode(for: passwordData, using: symmetricKey)
-            return formatPassword(Data(passwordHashDigest))
+            let passwordAuthenticateDigest = HMAC<SHA512>.authenticationCode(for: passwordData, using: symmetricKey)
+            return formatPassword(Data(passwordAuthenticateDigest))
         }
     
-    
-    
-    
-//------------------------------------------------------------------------------
-/*----------------------------HASHING--------------------------------------
+
+/**----------------------------HASHING--------------------------------------
  
---------------- Hashing algorithm used to convert text of any length into a fixed-size string.
---------------- Each output produces a SHA-512 length of 512 bits (64 bytes). This algorithm is commonly used for email addresses hashing, password hashing, and digital record verification.
+ ## Hashing algorithm used to convert text of any length into a fixed-size string.
+ 
+  ## Each output produces a SHA-512 length of 512 bits (64 bytes). This algorithm is commonly used for email addresses hashing, password hashing, and digital record verification.
  
 ----------------------------HASHING----------------------------------------*/
 
 
-        // CrytoKit
+    /**
+     
+     Creates a hashSha512CryptoKit for Hashing.
+     
+     - Parameter passowrdString: data which you want to Hash.
+     - Returns: Hashing data into string format `passwordHashDigest`.
+     
+    */
+    
         public func hashSha512CryptoKit() -> String? {
             // Create the hash
             let passwordData = passowrdString.data(using: .utf8)!
@@ -97,18 +136,28 @@ public class CryptoKitClass {
         }
     
     
-    
-    
-    
-//------------------------------------------------------------------------------
-/*----------------------CREATING AND VERIFYING SIGNATURES-----------------------------
+
+/**----------------------CREATING AND VERIFYING SIGNATURES-----------------------------
      
---------------- A signature that has been confirmed to be valid by a recipient, gives them a strong indication that the information was indeed created by the person claiming to have created it and hasn’t been tampered with
---------------- Common public-key cryptography use cases are encryption and digital signatures.
---------------- CryptoKit uses ECC algorithms exclusively. We can choose between the P256/P384/P521 ECC algorithm and the Curve25519 ECC algorithm. They both have approximately the same security level and small key sizes.
+ ## A signature that has been confirmed to be valid by a recipient, gives them a strong indication that the information was indeed created by the person claiming to have created it and hasn’t been tampered with
+ 
+ ## Common public-key cryptography use cases are encryption and digital signatures.
+ 
+ ## CryptoKit uses ECC algorithms exclusively. We can choose between the P256/P384/P521 ECC algorithm and the Curve25519 ECC algorithm. They both have approximately the same security level and small key sizes.
  
      
 ----------------------CREATING AND VERIFYING SIGNATURES-----------------------------*/
+    
+    /**
+     
+     Creates a senderSignatureCurve25519 for Creating sender's signature.
+     `senderSigningPrivateKey` Sender uses a private key to sign.
+     
+     - Returns: The rawRepresentation of the public key is of type Data, so you can send it over the network with is  `publicKey`.
+     - Returns: The signature for the data `signature`.
+     - Returns: The output of a Secure Hashing Algorithm 2 (SHA-2) hash with a 512-bit digest `SHA512Digest`.
+     
+    */
     
     
     public func senderSignatureCurve25519() -> (Data, Data, SHA512Digest) {
@@ -116,8 +165,7 @@ public class CryptoKitClass {
        
         let senderSigningPublicKeyData =
         senderSigningPrivateKey.publicKey.rawRepresentation
-        //The rawRepresentation of the public key is of type Data, so you can send it over the network.
-
+       
         let data = passowrdString.data(using: .utf8)!
         let digest512 = SHA512.hash(data: data)
         let signatureForDigest = try! senderSigningPrivateKey.signature(
@@ -127,6 +175,18 @@ public class CryptoKitClass {
         return (senderSigningPublicKeyData, signatureForDigest, digest512)
 
     }
+    
+    /**
+     
+     Creates a receiverVerifySignatureCurve25519 for verifying signature to recevier.
+     
+     - Parameter publicKey: Sender’s public key to verify.
+     - Parameter signature: The signature of sender.
+     - Parameter SHA512Digest: The output of a Secure Hashing Algorithm 2 (SHA-2) hash with a 512-bit digest..
+     
+     - Returns: Verification of senders indentity, and data if verification is successfull it will return true else false.
+     
+    */
     
     
     public func receiverVerifySignatureCurve25519(publicKey : Data, signature: Data, SHA512Digest: SHA512Digest) -> Bool {
@@ -142,19 +202,43 @@ public class CryptoKitClass {
         }
     }
     
-//------------------------------------------------------------------------------
-/*------------------------------RSA ALGO-----------------------------
---------------------------------RSA ALGO---------------------------------------*/
     
+    
+    /**
+     
+     Creates a generateKeyPair for verifying signature to recevier.
+     
+     - Parameter publicKeyTag: The corresponding value is of type CFData and contains public tag data.
+     - Parameter privateKeyTag: The corresponding value is of type CFData and contains private tag data.
+     - Parameter keySize: A key whose value indicates the size of the generated key pair. Set to 256 bits.
+     
+     - Returns: It will return `publicKey` and `privateKey`.
+     
+    
+
+     
+    */
     
     
     public func generateKeyPair(publicKeyTag: String, privateKeyTag:String, keySize: Int) -> KeyPair?  {
 
-       // A key whose value indicates the item's private tag.
+        // A key whose value indicates the item's private tag.
+        //`kSecAttrIsPermanent`: A key whose value is a boolean. If true, it indicates that the cryptographic key should be stored in the default keychain at creation time.
+        
+        //`kSecAttrApplicationTag`: A key whose value is an attribute with a unique NSData value so that you can find and retrieve the private key later on from the key chain. It’s constructed from a string and preferable to be in reverse DNS notation.
+
+        
+        
         let privateAttributes = [String(kSecAttrIsPermanent): true,
                                  String(kSecAttrApplicationTag): privateKeyTag] as [String : Any]
         let publicAttributes = [String(kSecAttrIsPermanent): true,
                                 String(kSecAttrApplicationTag): publicKeyTag] as [String : Any]
+        
+        //`kSecAttrKeyType` :  A key whose value indicates the type of cryptography algorithm used to produce the key pair (public and private keys). Set to elliptic curve algorithm (ECC).
+        
+        //`kSecAttrKeySizeInBits` : A key whose value indicates the size of the generated key pair. Set to 256 bits.
+        
+        //`kSecPrivateKeyAttrs` : A key whose value is set to the created sub-dictionary that does contain the specifications of the private key.
 
         let pairAttributes = [String(kSecAttrKeyType): kSecAttrKeyTypeRSA,
                               String(kSecAttrKeySizeInBits): keySize,
@@ -171,17 +255,54 @@ public class CryptoKitClass {
     }
     
     
-   public func callEncryptionUsingRSA(publicKey : SecKey, encryptData : String) -> Data {
+/**----------------------RSA  ALGORITHM FOR ENCRYPTION/DECRYPTION-----------------------------
+         
+     ##  RSA algorithm is asymmetric cryptography algorithm. Asymmetric actually means that it works on two different keys i.e. Public Key and Private Key. As the name describes that the Public Key is given to everyone and Private key is kept private.
+     
+         
+----------------------CREATING AND VERIFYING SIGNATURES-----------------------------*/
+    
+
+    /**
+     
+     
+     Creates a callEncryptionUsingRSA for verifying signature to recevier.
+     
+     Utilize the `SecKeyCreateEncryptedData` security API to encrypt the message data. You pass public key and the encryption algorithm (SecKeyAlgorithm) for it to be utilized.
+     
+     - Parameter publicKey: Public key for sender.
+     - Parameter encryptData: Message to encrypt.
+     
+     - Returns: It will return encrypted data as `ciphertext`.
+     
+    */
+    
+    
+   public func callEncryptionUsingRSA(publicKey : SecKey, messageData : String) -> Data {
         
         
         var error: Unmanaged<CFError>?
-        let message = Data(encryptData.utf8)
+        let message = Data(messageData.utf8)
         let ciphertext = SecKeyCreateEncryptedData(publicKey, .rsaEncryptionPKCS1, message as CFData, &error)! as Data
         
         return ciphertext
     }
   
-  
+    
+    /**
+     
+     Creates a callEncryptionUsingRSA for verifying signature to recevier.
+     
+     Get the encrypted message string & transform the encrypted message to be of type Data.
+     
+     Utilize the `SecKeyCreateDecryptedData` security API to decrypt the encrypted message data. You pass to it the created private key and the encryption algorithm (SecKeyAlgorithm) used before for encryption
+     
+     - Parameter encyrptedData: Encrypted data.
+     - Parameter privateKey: Receivers private key for decryption.
+     
+     - Returns: It will return decrypted data as `decryptedText`.
+     
+    */
     
    public func callDecryptionUsingRSA(encyrptedData: Data, privateKey: SecKey) -> String {
         
